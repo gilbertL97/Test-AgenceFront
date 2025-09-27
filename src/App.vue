9cf4ff68d9509d6a06b813cf8dbc72dcd2f9d4f7
Gilbert Luis <gilbertluis97@gmail.com>
2025-09-27 18:02:41 -0300

    starting front

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons-vue';
+import { MenuProps } from 'ant-design-vue';
+</script>
+
+<template>
+  <a-menu v-model:selectedKeys="current" mode="horizontal" :items="items" />
+</template>
+
+<style scoped></style>
