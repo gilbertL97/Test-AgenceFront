39133ddd40cac0111f4e9b8c898696a5cbdc21e8
Gilbert Luis <gilbertluis97@gmail.com>
2025-09-30 19:24:01 -0300

    fixing style

--- src/components/consultComponent.vue
+++ src/components/consultComponent.vue
@@ -42,7 +42,10 @@ const isButtonsEnabled = computed(() => {
     <div class="ml-15">
       <a-range-picker v-model:value="dateRange" picker="month" format="MMMM-YYYY" valueFormat="YYYY-MM" />
     </div>
-    <div class="flex mt-2 items-center justify-center">
+    <div v-if="isLoading" class="flex items-center justify-center h-full w-full">
+      <a-spin size="large" />
+    </div>
+    <div v-else class="flex mt-2 items-center justify-center">
       <TransferCutsom :is-loading="isLoading" :data="data" @selected-consultors="verifiConsultors" />
       <div class="flex flex-col ml-10 gap-8 ">
         <a-button class="p-2" :disabled="!isButtonsEnabled" @click="handleAction('relatorio')"> <template #icon>
